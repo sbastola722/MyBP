b6cca92a0609363005a5e4334162cf8717bec8d9
Suraj Bastola <suraj@SurajMB.local>
2024-01-03 17:35:56 -0700

    initial setup

---
+++ MyBP/MyBPApp.swift
@@ -0,0 +1,19 @@
+//
+//  MyBPApp.swift
+//  MyBP
+//
+//  Created by Suraj Bastola on 12/22/23.
+//  Copyright © 2023 Suraj Bastola. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct MyBPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
